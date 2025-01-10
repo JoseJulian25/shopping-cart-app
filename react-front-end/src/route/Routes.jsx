ac82a734dd6fe9efc7e0593de23fe17508e090b5
Alexis <AlexisNoMain@icloud.com>
2025-01-10 00:01:10 -0400

    Relocating files

--- react-front-end/src/route/Routes.jsx
+++ react-front-end/src/route/Routes.jsx
@@ -1,15 +1,15 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import React, { Suspense } from 'react';
-import ProtectedRoute from "./route/ProtectedRoute";
-import Loading from './components/Loading/Loading'; 
+import ProtectedRoute from "./ProtectedRoute.jsx";
+import Loading from "../components/Loading/Loading.jsx";
 
-const AuthLayout = React.lazy(() => import('./layouts/AuthLayout'));
-const RegisterForm = React.lazy(() => import('./components/Register/RegisterForm'));
-const LoginForm = React.lazy(() => import('./components/LoginForm'));
-const RestrictRoute = React.lazy(() => import('./route/RestrictRoute'));
-const HomeLayout = React.lazy(() => import('./layouts/HomeLayout'));
-const ShoppingCartPage = React.lazy(() => import('./pages/ShoppingCartPage'));
-const CategorySection = React.lazy(() => import('./components/CategorySection/CategorySection'));
+const AuthLayout = React.lazy(() => import('../layouts/AuthLayout'));
+const RegisterForm = React.lazy(() => import('../components/Register/RegisterForm'));
+const LoginForm = React.lazy(() => import('../pages/LoginForm'));
+const RestrictRoute = React.lazy(() => import('./RestrictRoute'));
+const HomeLayout = React.lazy(() => import('../layouts/HomeLayout'));
+const ShoppingCartPage = React.lazy(() => import('../pages/ShoppingCartPage'));
+const CategorySection = React.lazy(() => import('../components/CategorySection/CategorySection'));
 
 const router = createBrowserRouter([
     {
